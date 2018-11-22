56cb3b009edb4343043130bd6e2c84b7eb7f4f85
rndev80 <rndev80@gmail.com>
2018-11-22 14:03:42 +0100

    send email using sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'rails-photo-app-sendgrid-api-key',
+    :password => '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',
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
